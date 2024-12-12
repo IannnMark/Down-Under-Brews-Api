059ef6a47e265debfd88eb5891872fb6932facc7
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-12 19:12:48 +0800

    add errorHandler

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
