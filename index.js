e99d328880ea0302d55df73fc4e2e117b9e8afbf
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-12 21:15:34 +0800

    setup index

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cloudinary = require("cloudinary");
+const authRoutes = require("./routes/auth");
 
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch(err => {
+        console.log(err);
+    });
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
+app.use("/api/auth", authRoutes);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
