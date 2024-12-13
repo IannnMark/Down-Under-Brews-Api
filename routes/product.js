2f01f88dabaaf9d411ba00d836fa609ee58b81c2
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 18:12:27 +0800

    add getSingleProduct route

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const upload = require("../utils/multer");
-const { createProduct, deleteProduct, updateProduct, getAdminProducts } = require("../controllers/productController.js");
+const { createProduct, deleteProduct, updateProduct, getAdminProducts, getSingleProduct } = require("../controllers/productController.js");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser.js");
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post("/admin/product/new", upload.array("imageUrls"), verifyToken, author
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct)
 router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
 router.get("/admin/products", verifyToken, authorizeRoles("admin"), getAdminProducts);
+router.get("/admin/product/:id", verifyToken, authorizeRoles("admin"), getSingleProduct);
+
 
 module.exports = router;
