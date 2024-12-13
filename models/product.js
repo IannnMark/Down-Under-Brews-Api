872f89ed99a52daec09651f5f81e343a1bf636e9
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 12:37:51 +0800

    complete product model

---
+++ models/product.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    origin: {
+        type: String,
+        required: true,
+    },
+    roastLevel: {
+        type: String,
+        enum: ["Light", "Medium", "Dark"],
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    availableWeights: {
+        type: [String],
+        required: true,
+    },
+    availability: {
+        type: Boolean,
+        required: true,
+    },
+    processingMethod: {
+        type: String,
+        enum: ["Washed", "Natural", "Honey-Processed"],
+        required: true,
+    },
+    harvestSeason: {
+        type: String,
+        required: false,
+    },
+    certifications: {
+        type: String,
+        required: false,
+    },
+    imageUrls: {
+        type: Array,
+        required: true,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+    deletedAt: {
+        type: Date,
+        default: null,
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("Product", productSchema);
