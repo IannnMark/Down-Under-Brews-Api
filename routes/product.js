37ae00d68bf4e972513960c4e14eb4a3fc859766
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 18:05:19 +0800

    add getAdminProducts route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const upload = require("../utils/multer");
-const { createProduct, deleteProduct, updateProduct } = require("../controllers/productController.js");
+const { createProduct, deleteProduct, updateProduct, getAdminProducts } = require("../controllers/productController.js");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser.js");
 const router = express.Router();
 
 router.post("/admin/product/new", upload.array("imageUrls"), verifyToken, authorizeRoles("admin"), createProduct);
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct)
 router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
+router.get("/admin/products", verifyToken, authorizeRoles("admin"), getAdminProducts);
 
 module.exports = router;
