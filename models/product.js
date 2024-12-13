1e524aef8ba4f12af67991f4192ef5e953112a58
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 14:34:50 +0800

    modify the product model

--- models/product.js
+++ models/product.js
@@ -9,6 +9,10 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    category: {
+        type: String,
+        required: true,
+    },
     origin: {
         type: String,
         required: true,
