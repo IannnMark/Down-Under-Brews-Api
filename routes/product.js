2776895cf321895668bed78edea919bb1cc410dd
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 18:01:36 +0800

    add updateProduct route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const upload = require("../utils/multer");
-const { createProduct, deleteProduct } = require("../controllers/productController.js");
+const { createProduct, deleteProduct, updateProduct } = require("../controllers/productController.js");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser.js");
 const router = express.Router();
 
 router.post("/admin/product/new", upload.array("imageUrls"), verifyToken, authorizeRoles("admin"), createProduct);
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct)
-
+router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
 
 module.exports = router;
