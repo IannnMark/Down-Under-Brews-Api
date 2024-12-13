26a1faf089d3bb9c7fe35f14a176ddcea8eff5ae
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-13 18:00:14 +0800

    add update product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -40,3 +40,47 @@ exports.deleteProduct = async (req, res, next) => {
     }
 }
 
+exports.updateProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return next(errorHandler(404, "Product not found"));
+    }
+    try {
+        if (req.files && req.files.length > 0) {
+            if (product.imageUrls && Array.isArray(product.imageUrls)) {
+                for (const image of product.imageUrls) {
+                    if (image.public_id) {
+                        await cloudinary.uploader.destroy(image.public_id);
+                    }
+                }
+            }
+
+            const imagesLinks = [];
+            for (const file of req.files) {
+                const result = await cloudinary.v2.uploader.upload(file.path, {
+                    folder: "Products",
+                });
+                imagesLinks.push({
+                    url: result.secure_url,
+                });
+            }
+
+            res.body.imageUrls = imagesLinks;
+        } else {
+            req.body.imageUrls = product.imageUrls;
+
+        }
+
+        const updateProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.status(200).json(updateProduct);
+
+    } catch (error) {
+        next(error);
+    }
+}
