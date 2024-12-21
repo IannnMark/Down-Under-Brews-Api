876d9b07231e2158e981975db89a2d48dac63b7b
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-21 14:20:10 +0800

    fix: update route for getting a single product

--- routes/product.js
+++ routes/product.js
@@ -8,7 +8,7 @@ router.post("/admin/product/new", upload.array("imageUrls"), verifyToken, author
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct)
 router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
 router.get("/admin/products", verifyToken, authorizeRoles("admin"), getAdminProducts);
-router.get("/admin/product/:id", verifyToken, authorizeRoles("admin"), getSingleProduct);
+router.get("/product/:id", getSingleProduct);
 router.get("/get/products", getProducts);
 
 module.exports = router;
