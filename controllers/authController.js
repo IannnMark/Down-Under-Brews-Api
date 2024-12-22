fcc8fb9bb80f36c4595cf2280c9e659d92c148af
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-22 14:17:58 +0800

    feat: add Google authentication to authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,3 +50,41 @@ exports.signOut = async (req, res, next) => {
         next(error);
     }
 }
+
+exports.google = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (user) {
+            const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET)
+            const { password: pass, ...rest } = user._doc;
+            res
+                .cookie("access_token", token, {
+                    secure: process.env.NODE_ENV === "production",
+                    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+                    maxAge: 24 * 60 * 60 * 1000,
+                })
+                .status(200)
+                .json(rest);
+        } else {
+            const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
+            const newUser = new User({
+                username: req.body.username.split(" ").join(" ").toLowerCase() +
+                    Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword,
+                avatar: req.body.photo,
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const { password: pass, ...rest } = newUser._doc;
+            res
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                })
+                .status(200)
+                .json(rest);
+        }
+    } catch (error) {
+        next(error);
+    }
+}
