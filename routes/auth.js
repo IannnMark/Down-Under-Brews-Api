c7b3efed294df278064d9cd3e2f2e317cadc6d2a
ianmark8 <ianmarkmorga07@gmail.com>
2024-12-22 14:18:31 +0800

    feat: add Google authentication route to auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signUp, signIn, signOut } = require("../controllers/authController");
+const { signUp, signIn, signOut, google } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
 router.get("/logout", signOut);
+router.post("/google", google);
 
 module.exports = router;
