2eda3488a70eca33ec1bfad7382109f7eb85d445
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-02 18:30:52 +0800

    feat: add cors package import to index.js for CORS support

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cookieParser = require("cookie-parser");
 const cloudinary = require("cloudinary");
 const authRoutes = require("./routes/auth");
 const productRoutes = require("./routes/product");
+const cors = require("cors");
 
 dotenv.config();
 
