eff33d2d27743250a25bb261f4a650ce49afad9a
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-04 20:46:43 +0800

    add deleteUser route to user.js for account deletion functionality

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { updateUser } = require("../controllers/userController");
+const { updateUser, deleteUser } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 module.exports = router;
