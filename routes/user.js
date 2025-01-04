695f4eb3d5de20a61995ebce2f51a1ab6eed950a
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-04 21:15:36 +0800

    add route for retrieving archived users

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { updateUser, deleteUser, allUsers, eraseUser, archivedUser, restoreUser } = require("../controllers/userController");
+const { updateUser, deleteUser, allUsers, eraseUser, archivedUser, restoreUser, getAdminArchivedUsers } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
@@ -10,6 +10,6 @@ router.get("/admin/users", verifyToken, authorizeRoles("admin"), allUsers);
 router.get("/admin/delete/:id", verifyToken, authorizeRoles("admin"), eraseUser);
 router.delete("/admin/user/archived/:id", verifyToken, authorizeRoles("admin"), archivedUser);
 router.put("/admin/user/restore-user/:id", verifyToken, authorizeRoles("admin"), restoreUser);
-
+router.get("/admin/archived-users", verifyToken, authorizeRoles("admin"), getAdminArchivedUsers);
 
 module.exports = router;
