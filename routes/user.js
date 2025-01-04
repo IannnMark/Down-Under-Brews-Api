e2f9448dbb97e91ab23e89aefe7d3e1c4ba31bac
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-04 21:13:35 +0800

    add routes for archiving and restoring users in userController

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { updateUser, deleteUser, allUsers, eraseUser } = require("../controllers/userController");
+const { updateUser, deleteUser, allUsers, eraseUser, archivedUser, restoreUser } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../utils/verifyUser");
 
 const router = express.Router();
@@ -8,5 +8,8 @@ router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/admin/users", verifyToken, authorizeRoles("admin"), allUsers);
 router.get("/admin/delete/:id", verifyToken, authorizeRoles("admin"), eraseUser);
+router.delete("/admin/user/archived/:id", verifyToken, authorizeRoles("admin"), archivedUser);
+router.put("/admin/user/restore-user/:id", verifyToken, authorizeRoles("admin"), restoreUser);
+
 
 module.exports = router;
