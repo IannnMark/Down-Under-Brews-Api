17a078179b34f97e2c8cc6530682ca96b3681bef
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-05 23:46:28 +0800

    fix: add image upload handling to product update route

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post("/admin/product/new", upload.array("imageUrls"), verifyToken, authorizeRoles("admin"), createProduct);
 router.delete("/admin/product/delete/:id", verifyToken, authorizeRoles("admin"), deleteProduct)
-router.put("/admin/product/update/:id", verifyToken, authorizeRoles("admin"), updateProduct);
+router.put("/admin/product/update/:id", upload.array("imageUrls"), verifyToken, authorizeRoles("admin"), updateProduct);
 router.get("/admin/products", verifyToken, authorizeRoles("admin"), getAdminProducts);
 router.get("/product/:id", getSingleProduct);
 router.get("/get/products", getProducts);
