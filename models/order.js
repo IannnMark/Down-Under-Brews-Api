7193511d8804a56b8669ba6ae55b0912b498ef7b
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-07 14:49:51 +0800

    create order model with schema for managing orders

---
+++ models/order.js
@@ -0,0 +1,98 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    orderItems: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                required: true,
+                ref: "Product"
+            },
+            weight: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    shippingAddress: {
+        fullName: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        city: {
+            type: String,
+            required: true,
+        },
+        postalCode: {
+            type: String,
+            required: true,
+        },
+        country: {
+            type: String,
+            required: true,
+        },
+    },
+    paymentInfo: {
+        method: {
+            type: String,
+            enum: ["PayPal", "Cash on Delivery"],
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["Pending", "Paid", "Failed"],
+            default: "Pending",
+        },
+    },
+    orderStatus: {
+        type: String,
+        enum: ["Processing", "Shipped", "Delivered", "Cancelled"],
+        default: "Processing",
+    },
+    totalPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    shippingPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    taxPrice: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    deliveredAt: {
+        type: Date,
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false,
+    },
+    deletedAt: {
+        type: Date,
+        default: null,
+    }
+}, { timestamps: true });
+
+
+module.exports = mongoose.model("Order", orderSchema);
