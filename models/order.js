706d0d7cacbb487870b0924df305a3c8172a8a93
ianmark8 <ianmarkmorga07@gmail.com>
2025-01-09 09:44:15 +0800

    refactor: remove taxPrice field from order schema

--- models/order.js
+++ models/order.js
@@ -76,11 +76,6 @@ const orderSchema = new mongoose.Schema({
         required: true,
         default: 0,
     },
-    taxPrice: {
-        type: Number,
-        required: true,
-        default: 0,
-    },
     deliveredAt: {
         type: Date,
     },
